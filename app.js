278a5367df8637bf4c01f0fd6c65a970ad8e1e1e
ianng105 <champian.ng@gmail.com>
2025-11-18 19:38:33 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ const app=express();
 const PORT = process.env.PORT || 8080;
 // 设置 EJS 视图引擎
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'view'));
 
 // 托管静态文件（将你的 welcome.css / welcome.js / 以及 register_page.html, login.html 放到 public/ 下）
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 根路由，渲染 welcome.ejs
+
 app.get('/', (req, res) => {
-  res.render('welcome');
+  res.redirect('./welcome');
 });
+
+app.get('/welcome', (req, res) => {
+  res.status(200).render('welcome');
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 
