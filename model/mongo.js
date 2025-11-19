82dedd08a5fdaf5b5226f704ea4da39c517f87f1
huil25514-gif <huil25514@gmail.com>
2025-11-19 14:42:44 +0800

    mongo.js

--- model/mongo.js
+++ model/mongo.js
@@ -4,3 +4,24 @@ const { MongoClient } = require('mongodb');
 const uri = “mongodb+srv://huil25514_db_user:<1234>@cluster0.96ftjs5.mongodb.net/?appName=Cluster0”
 const client = new MonogoClient(uri);
 
+let db;
+
+async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db('healthypal');
+    console.log('Connected to MongoDB');
+    return db;
+  } catch (err) {
+    console.error('DB connection error:', err);
+    throw err;
+  }
+}
+
+function getDB() {
+  return db;
+}
+
+
+module.exports = { connectDB, getDB };
+
