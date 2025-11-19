8ab95b3d5cdfc678c7bce6864d98fe297f02b5b9
huil25514-gif <huil25514@gmail.com>
2025-11-19 14:19:53 +0800

    mongo.js

--- model/mongo.js
+++ model/mongo.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
 
-const uri = 
+const uri = “mongodb+srv://huil25514_db_user:<1234>@cluster0.96ftjs5.mongodb.net/?appName=Cluster0”
 const client = new MonogoClient(uri);
 
