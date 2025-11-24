1cf277198f73e265e89208ec1eab47829c19b542
boyang200405-cmd <boyang200405@gmail.com>
2025-11-24 14:28:20 +0800

    Add files via upload

--- model/user.js
+++ model/user.js
@@ -1,4 +1,4 @@
-const { MongoClient, ObjectId } = require('mongodb');
+const { ObjectId } = require('mongodb');
 const { getDB } = require("./mongo");
 const collectionName = "user";
 
@@ -9,11 +9,17 @@ class User {
     return await db.collection(collectionName);
   }
 
-  // 注册用户
+  // 注册用户（含默认头像和简介）
   static async createUser(userData) {
     const collection = await User.getCollection();
-    const result = await collection.insertOne(userData);
-    return { ...userData, _id: result.insertedId };
+    // 新增默认头像和空简介
+    const userDataWithDefaults = {
+      ...userData,
+      avatar: userData.avatar || '/images/default-avatar.jpg', // 默认头像路径
+      profile: userData.profile || '' // 个人简介默认空
+    };
+    const result = await collection.insertOne(userDataWithDefaults);
+    return { ...userDataWithDefaults, _id: result.insertedId };
   }
 
   // 查询所有用户
@@ -22,21 +28,24 @@ class User {
     return await collection.find().toArray();
   }
 
-  // 根据ID查询用户 - 修复版
+  // 根据ID查询用户
   static async findUserById(id) {
     const collection = await User.getCollection();
     return await collection.findOne({ _id: new ObjectId(id) });
   }
 
+  // 根据用户名查询用户
   static async findUserByUsername(username) {
     const collection = await User.getCollection();
     return await collection.findOne({ username });
   }
 
+  // 根据邮箱查询用户
   static async findUserByEmail(email) {
     const collection = await User.getCollection();
     return await collection.findOne({ email });
   }
+
   // 更新用户信息
   static async updateUser(id, updateData) {
     const collection = await User.getCollection();
