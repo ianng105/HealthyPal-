81c5429d24d35d6dcb2594e4357874545d2b8815
wangyusen050213-ship-it <wangyusen050213@gmail.com>
2025-12-01 17:49:51 +0800

    Update listController.js

--- controller/listController.js
+++ controller/listController.js
@@ -1,7 +1,7 @@
 exports.add= (req, res) => {
   const { food_name, calories, serving_description, quantity } = req.body;
   if (!food_name || calories === undefined) {
-    return res.status(400).send('invaild input');
+    return res.status(400).send('缺少必要字段');
   }
   if (!req.session.eatenList) req.session.eatenList = [];
   req.session.eatenList.push({
@@ -11,19 +11,19 @@ exports.add= (req, res) => {
     serving_description: serving_description ? String(serving_description) : '',
     quantity: quantity ? Number(quantity) : 1
   });
-  res.redirect('back');
+  res.redirect('/searchFood');
 };
 
-
+// 从已吃列表移除食物
 exports.remove=(req, res) => {
   const { id } = req.body;
   if (req.session.eatenList) {
     req.session.eatenList = req.session.eatenList.filter(item => item.id !== id);
   }
-  res.redirect('back');
+  res.redirect('/searchFood');
 };
 
-
+// 查看已吃列表（调试用）
 exports.show=(req, res) => {
   const list = req.session.eatenList || [];
   const totalCalories = list.reduce((sum, item) => {
